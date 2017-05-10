e3a3552e96cb36bf8d41bc5e27d44387475ccd28
Hannah <hml1014@sbcglobal.net>
2017-05-09 22:59:00 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package mines;
+
+/*
+ * Hannah Lehr
+ * 2017
+ * 
+ * This Main class launches the Board GUI,
+ * which contains the Minesweeper game.
+ */
+
+public class Main {
+	public static void main(String args[]){
+		new Board();	
+	}
+}
